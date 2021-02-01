a6d6802404f4d4321ce3e0894b7be69c88ed26a0
hiwad rashad <hiwadrashad112@hotmail.com>
2021-02-01 15:33:15 +0100

    Add project files.

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+    public static class extensionclass
+    {
+        public static void Combine(this DateTime extensionparameter, DateTime datepart, DateTime timepart)
+        { 
+     
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime item = new DateTime();
+            item.Combine(new DateTime(), new DateTime());
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
